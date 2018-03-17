a6ca7b37771a2e85046b413596ea2438affa99a5
Petro Protsyk <piter.protsyk@gmail.com>
2018-03-17 16:19:54 +0100

    Unit Tests for term search

---
+++ Src/Protsyk.PMS.FullText.Core.UnitTests/SearchTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Protsyk.PMS.FullText.Core.UnitTests
+{
+    public class SearchTest : TestWithFolderBase
+    {
+        [Fact]
+        public void TestTermSearchPersistentIndex()
+        {
+            using (var index = TestHelper.PrepareIndexForSearch(new PersistentIndexName(TestFolder)))
+            {
+                TestTermSearch(index);
+            }
+        }
+
+        [Fact]
+        public void TestTermSearchMemoryIndex()
+        {
+            using (var index = TestHelper.PrepareIndexForSearch(new InMemoryIndexName()))
+            {
+                TestTermSearch(index);
+            }
+        }
+
+        private void TestTermSearch(IFullTextIndex index)
+        {
+            var postings = index.GetPostingList("this").ToString();
+            Assert.Equal("[3,1,1], [4,1,1], [5,1,1], [6,1,8]", postings);
+        }
+    }
+}
