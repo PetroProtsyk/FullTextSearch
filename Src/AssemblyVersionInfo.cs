3bd69544c1f358cd7a362937e764ca0a1444f0e8
Petro Protsyk <piter.protsyk@gmail.com>
2018-04-27 17:32:22 +0200

    Change version number

--- Src/AssemblyVersionInfo.cs
+++ Src/AssemblyVersionInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Petro Protsyk")]
 [assembly: AssemblyCopyright("Copyright © Petro Protsyk 2017-2018")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
