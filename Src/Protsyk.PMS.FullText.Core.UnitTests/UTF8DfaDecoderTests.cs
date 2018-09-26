43a441b307a14d0b84a51f79865194254faf5f59
Petro Protsyk <piter.protsyk@gmail.com>
2018-09-26 19:12:34 +0200

    UTF-8 DFA decoder

---
+++ Src/Protsyk.PMS.FullText.Core.UnitTests/UTF8DfaDecoderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Protsyk.PMS.FullText.Core.Common;
+using Xunit;
+
+namespace Protsyk.PMS.FullText.Core.UnitTests
+{
+    public class UTF8DfaDecoderTest
+    {
+        [Fact]
+        public void Decoding()
+        {
+            var text = "Hello Здоровенькі ᆵሄ⅙⅙Ⅸ ТестыТексты Були שלום עולם";
+            var input = Encoding.UTF8.GetBytes(text);
+            var decoded = UTF8DfaDecoder.Decode(input);
+            Assert.Equal(text, decoded);
+        }
+    }
+}
