86c3919bace3dd3f1a0ce25cce372bb1b6ed21d3
Petro Protsyk <piter.protsyk@gmail.com>
2018-12-29 10:13:55 +0100

    FST wildcard matching test

--- Src/Protsyk.PMS.FullText.Core.UnitTests/Automata/FiniteStateTransducerTests.cs
+++ Src/Protsyk.PMS.FullText.Core.UnitTests/Automata/FiniteStateTransducerTests.cs
@@ -113,5 +113,60 @@ namespace Protsyk.PMS.FullText.Core.Automata.UnitTests
                 Assert.Equal(outputs[i], v);
             }
         }
+
+        [Fact]
+        public void WildcardMatchingTest()
+        {
+            var inputs = new string[]{
+                "Albert Schweitzer Ziekenhuis. Locatie Amstelwijck Heliport",
+                "Amsterdam Airfield",
+                "Amsterdam Airport",
+                "Amsterdam Airport Schiphol",
+                "Amsterdam Heliport",
+                "Chafei Amsei Airport",
+                "New Amsterdam Airport",
+                "Schwarzheide/Schipkau Airport"
+            };
+
+            var outputs = new int[] {
+                43711,
+                23465,
+                41198,
+                2513,
+                43207,
+                5873,
+                41521,
+                29065
+            };
+
+            var fst = new FSTBuilder<int>(FSTVarIntOutput.Instance).FromList(inputs, outputs);
+            Verify(fst, inputs, outputs);
+
+            var expectedTerms = new string[]
+            {
+                "Amsterdam Airport Schiphol",
+                "Schwarzheide/Schipkau Airport"
+            };
+
+            var expectedOutputs = new int[]
+            {
+                2513,
+                29065
+            };
+
+            var expectedIndex = 0;
+
+            Assert.Equal(expectedOutputs.Length, expectedTerms.Length);
+
+            foreach (var term in fst.Match(new WildcardMatcher("*Schip*", 255)))
+            {
+                Assert.Equal(expectedTerms[expectedIndex], term);
+                Assert.True(fst.TryMatch(term, out int value));
+                Assert.Equal(expectedOutputs[expectedIndex], value);
+                ++expectedIndex;
+            }
+
+            Assert.Equal(expectedOutputs.Length, expectedIndex);
+        }
     }
 }
