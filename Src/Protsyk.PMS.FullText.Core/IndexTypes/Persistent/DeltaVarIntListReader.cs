930bb55f6fc0c7f022fb4aa536cc87347147b2d8
petro <petro@unknown.com>
2019-01-12 08:14:48 +0100

    Persistent list of delta encoded integers. Sequence of numbers in the list should monotonically increase, example: 1,3,4,5,10,..

---
+++ Src/Protsyk.PMS.FullText.Core/IndexTypes/Persistent/DeltaVarIntListReader.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Protsyk.PMS.FullText.Core.Common;
+using Protsyk.PMS.FullText.Core.Common.Persistance;
+
+namespace Protsyk.PMS.FullText.Core
+{
+    public class DeltaVarIntListReader : IDisposable
+    {
+        #region Fields
+        private readonly IPersistentStorage persistentStorage;
+        #endregion
+
+        public DeltaVarIntListReader(string folder, string fileNamePostingLists)
+            : this(new FileStorage(Path.Combine(folder, fileNamePostingLists)))
+        {
+        }
+
+        public DeltaVarIntListReader(IPersistentStorage storage)
+        {
+            this.persistentStorage = storage;
+        }
+
+        #region API
+        public IEnumerable<ulong> Get(long listStart)
+        {
+            return new DeltaListReaderImpl(persistentStorage, listStart, 0UL);
+        }
+
+        public IEnumerable<ulong> GetLowerBound(long listStart, ulong value)
+        {
+            return new DeltaListReaderImpl(persistentStorage, listStart, value);
+        }
+        #endregion
+
+        #region ReaderEnumerator
+        private class DeltaListReaderImpl : IEnumerable<ulong>
+        {
+            private readonly IPersistentStorage storage;
+            private readonly long listStart;
+            private readonly ulong firstValue;
+
+            public DeltaListReaderImpl(IPersistentStorage storage, long listStart, ulong firstValue)
+            {
+                this.storage = storage;
+                this.listStart = listStart;
+                this.firstValue = firstValue;
+            }
+
+            public IEnumerator<ulong> GetEnumerator()
+            {
+                return new ReaderEnumerator(storage, listStart, firstValue);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        private class ReaderEnumerator : IEnumerator<ulong>
+        {
+            #region Fields
+            private const int SkipSearchBlocksThreshold = 8;
+            private static readonly int HeaderLength = 1 + sizeof(int) + sizeof(int);
+            private readonly IPersistentStorage persistentStorage;
+            private readonly long listStart;
+            private readonly byte[] buffer;
+            private readonly ulong firstValue;
+            private long readOffset;
+            private int dataInBuffer;
+            private int indxInBuffer;
+            private bool isEof;
+            private long listEndOffset;
+            private ulong current;
+            private int state;
+            #endregion
+
+            #region Methods
+            public ReaderEnumerator(IPersistentStorage storage, long listStart, ulong firstValue)
+            {
+                this.persistentStorage = storage;
+                this.listStart = listStart;
+                this.state = 0;
+                this.firstValue = firstValue;
+                this.buffer = new byte[DeltaVarIntListWriter.BlockSize];
+                Reset();
+            }
+
+            private ulong NextNumber()
+            {
+                if (indxInBuffer >= dataInBuffer)
+                {
+                    var toRead = buffer.Length;
+                    if (listEndOffset - readOffset < toRead)
+                    {
+                        checked
+                        {
+                            toRead = (int)(listEndOffset - readOffset);
+                        }
+                    }
+
+                    if (toRead == 0)
+                    {
+                        isEof = true;
+                        return 0;
+                    }
+
+                    persistentStorage.ReadAll(readOffset, buffer, 0, toRead);
+
+                    for (int i=toRead; i<buffer.Length; ++i)
+                    {
+                        buffer[i] = 0;
+                    }
+
+                    readOffset += toRead;
+                    dataInBuffer = toRead;
+                    indxInBuffer = 0;
+                }
+
+                indxInBuffer += VarInt.ReadVUInt64(buffer, indxInBuffer, out var result);
+                return result;
+            }
+            #endregion
+
+            #region IEnumerator
+            public ulong Current => current;
+
+            object IEnumerator.Current => current;
+
+            public bool MoveNext()
+            {
+                while (true)
+                {
+                    if (state == 0)
+                    {
+                        if (isEof)
+                        {
+                            return false;
+                        }
+
+                        var header = new byte[HeaderLength];
+                        persistentStorage.ReadAll(readOffset, header, 0, header.Length);
+
+                        if (header[0] != (byte)'L')
+                        {
+                            throw new Exception("Invalid List Header");
+                        }
+
+                        var listLength = BitConverter.ToInt32(header, 1);
+                        var recordCount = BitConverter.ToInt32(header, 1 + sizeof(int));
+
+                        listEndOffset = readOffset + HeaderLength + listLength;
+
+                        readOffset += header.Length;
+                        state = 17;
+                    }
+
+                    if (state == 17)
+                    {
+                        // Seek state
+                        var left = (listEndOffset - readOffset) / buffer.Length;
+                        if (left < SkipSearchBlocksThreshold || !(current < firstValue))
+                        {
+                            state = 1;
+                        }
+                        else
+                        {
+                            // Allocate space for two blocks, find two consecutive blocks that contain
+                            // occurrence that is requested
+                            var t = new byte[buffer.Length * 2];
+                            var a = 0L;
+                            var b = left;
+                            var mid = a;
+                            while (a != b)
+                            {
+                                mid = (a + b) >> 1;
+
+                                // Read two blocks
+                                var midOffset = readOffset + buffer.Length * mid;
+                                var inList = (listEndOffset - midOffset - buffer.Length);
+                                var toRead = inList > t.Length ? t.Length : (int)inList;
+                                persistentStorage.ReadAll(midOffset, t, 0, toRead);
+
+                                // Block 1: Full value
+                                var j = 0;
+                                j += VarInt.ReadVUInt64(t, j, out var v1);
+
+                                // Block 2: Full value
+                                j = buffer.Length;
+                                j += VarInt.ReadVUInt64(t, j, out var v2);
+
+                                if (v1 < firstValue)
+                                {
+                                     if (v2 >= firstValue)
+                                     {
+                                         break;
+                                     }
+                                     else
+                                     {
+                                         a = mid + 1;
+                                     }
+                                }
+                                else
+                                {
+                                    b = mid;
+                                }
+                            }
+
+                            state = 1;
+                            indxInBuffer = 0;
+                            dataInBuffer = 0;
+                            readOffset   = readOffset + buffer.Length * mid;
+                        }
+                    }
+
+                    if (state == 1)
+                    {
+                        current = NextNumber();
+                        state = 2;
+
+                        if (!(current < firstValue))
+                        {
+                            return true;
+                        }
+                    }
+
+                    if (state == 2)
+                    {
+                        if (indxInBuffer >= dataInBuffer)
+                        {
+                            if (readOffset < listEndOffset)
+                            {
+                                // Next block
+                                state = 1;
+                                continue;
+                            }
+                        }
+
+                        var delta = NextNumber();
+                        if (delta == 0)
+                        {
+                            if (readOffset < listEndOffset)
+                            {
+                                if (indxInBuffer < dataInBuffer)
+                                {
+                                    // Case when buffer is filled with zeros at the end
+                                    indxInBuffer = dataInBuffer;
+                                }
+                                state = 1; // Next block
+                            }
+                            else
+                            {
+                                state = 0; // EOF
+                            }
+                            continue;
+                        }
+
+                        current += delta;
+                        if (!(current < firstValue))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            public void Reset()
+            {
+                state = 0;
+                dataInBuffer = 0;
+                indxInBuffer = 0;
+                readOffset = listStart;
+                isEof = false;
+                listEndOffset = 0;
+            }
+
+            public void Dispose()
+            {
+            }
+            #endregion
+        }
+        #endregion
+
+        #region IDisposable
+        public void Dispose()
+        {
+            persistentStorage?.Dispose();
+        }
+        #endregion
+    }
+}
---
+++ Src/Protsyk.PMS.FullText.Core/IndexTypes/Persistent/DeltaVarIntListWriter.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using Protsyk.PMS.FullText.Core.Common;
+using Protsyk.PMS.FullText.Core.Common.Persistance;
+
+namespace Protsyk.PMS.FullText.Core
+{
+    // Encodes monotonic sequence of integers into compressed binary representation
+    // using delta compression and VarInt encoding.
+    public class DeltaVarIntListWriter : IDisposable
+    {
+        #region Fields
+         // Should be at least MaxVarInt
+        internal static readonly int BlockSize = 1024;
+        private readonly byte[] buffer;
+        private readonly IPersistentStorage persistentStorage;
+
+        private int bufferIndex;
+        private ulong previous;
+        private long listStart;
+        private long totalSize;
+        private int recordCount;
+        private bool first;
+        #endregion
+
+        #region Constructors
+        public DeltaVarIntListWriter(string folder, string fileName)
+            : this(new FileStorage(Path.Combine(folder, fileName)))
+        {
+        }
+
+        public DeltaVarIntListWriter(IPersistentStorage storage)
+        {
+            this.buffer = new byte[BlockSize + VarInt.GetByteSize(ulong.MaxValue)];
+            this.persistentStorage = storage;
+        }
+        #endregion
+
+        #region API
+        public long StartList()
+        {
+            recordCount = 0;
+            bufferIndex = 0;
+            totalSize = 0;
+            previous = 0UL;
+            first = true;
+
+            listStart = persistentStorage.Length;
+
+            // Write List mark
+            persistentStorage.WriteAll(listStart, new byte[] {(byte)'L'}, 0, 1);
+
+            // Reserve space for the length of the list
+            persistentStorage.WriteAll(listStart + 1, BitConverter.GetBytes(0), 0, sizeof(int));
+
+            // Reserve space for the record count
+            persistentStorage.WriteAll(listStart + 1 + sizeof(int), BitConverter.GetBytes(0), 0, sizeof(int));
+
+            return listStart;
+        }
+
+        public void AddValue(ulong value)
+        {
+            if (value == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            if (first)
+            {
+                bufferIndex += VarInt.WriteVUInt64(value, buffer, bufferIndex);
+                previous = value;
+                first = false;
+            }
+            else
+            {
+                if (value <= previous)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+
+                var previousIndex = bufferIndex;
+                bufferIndex += VarInt.WriteVUInt64((ulong)(value - previous), buffer, bufferIndex);
+
+                if (bufferIndex > BlockSize)
+                {
+                    // Current value spans blocks:
+                    // 1) Reset index to previous value
+                    // 2) Flush block
+                    // 3) Start new block and write full occurrence to a new block
+                    bufferIndex = previousIndex;
+                    FlushBlock(false);
+                    bufferIndex = VarInt.WriteVUInt64(value, buffer, 0);
+                }
+
+                previous = value;
+            }
+            ++recordCount;
+        }
+
+        public long EndList()
+        {
+            if (bufferIndex > 0)
+            {
+                FlushBlock(true);
+            }
+
+            // Write length of the list
+            persistentStorage.WriteAll(listStart + 1, BitConverter.GetBytes(totalSize), 0, sizeof(int));
+
+            // Write record count of the list
+            persistentStorage.WriteAll(listStart + 1 + sizeof(int), BitConverter.GetBytes(recordCount), 0, sizeof(int));
+
+            var listEnd = persistentStorage.Length;
+
+            if (listEnd - listStart != totalSize + 1 + sizeof(int) + sizeof(int))
+            {
+                throw new InvalidOperationException();
+            }
+
+            return listEnd;
+        }
+
+        private void FlushBlock(bool last)
+        {
+            // Fill un-used space with zeros (waste)
+            for (int i=bufferIndex; i<BlockSize; ++i)
+            {
+                buffer[i] = 0;
+            }
+
+            int writeSize = last ? bufferIndex : BlockSize;
+            persistentStorage.WriteAll(persistentStorage.Length, buffer, 0, writeSize);
+            totalSize += writeSize;
+        }
+
+        public void Dispose()
+        {
+            persistentStorage?.Dispose();
+        }
+
+        #endregion
+
+        #region Autotest
+        public static void Test()
+        {
+            int N = 1000000;
+            int variation = 10000000;
+            long listStart = -1;
+
+            var timer = Stopwatch.StartNew();
+            var ar = new ulong[N];
+            using (var mem = new MemoryStorage())
+            {
+                using(var writer = new DeltaVarIntListWriter(mem.GetReference()))
+                {
+                    var s1 = writer.StartList();
+                    var prev = 0UL;
+                    var r = new Random(2019);
+                    for (int i = 0; i<N; ++i)
+                    {
+                        prev += 1 + (ulong)r.Next(variation);
+                        ar[i] = prev;
+                        writer.AddValue(prev);
+                    }
+                    var e1 = writer.EndList();
+                    Console.WriteLine($"Count  : {N}");
+                    Console.WriteLine($"Length : {e1 - s1}");
+
+                    listStart = s1;
+                }
+                Console.WriteLine($"Write  : {timer.Elapsed}");
+
+                timer = Stopwatch.StartNew();
+                using(var reader = new DeltaVarIntListReader(mem.GetReference()))
+                {
+                    int i = 0;
+                    foreach (var v in reader.Get(listStart))
+                    {
+                        if (ar[i] != v)
+                        {
+                            throw new Exception($"{ar[i]} != {v} at {i}");
+                        }
+                        ++i;
+                    }
+                }
+                Console.WriteLine($"Read   : {timer.Elapsed}");
+
+                timer = Stopwatch.StartNew();
+                using(var reader = new DeltaVarIntListReader(mem.GetReference()))
+                {
+                    for (int i = 0; i<N; ++i)
+                    {
+                        var v = reader.GetLowerBound(listStart, ar[i]).First();
+                        if (ar[i] != v)
+                        {
+                            throw new Exception($"{ar[i]} != {v} at {i}");
+                        }
+                    }
+                }
+                Console.WriteLine($"Seek   : {timer.Elapsed}");
+            }
+        }
+        #endregion
+    }
+}
