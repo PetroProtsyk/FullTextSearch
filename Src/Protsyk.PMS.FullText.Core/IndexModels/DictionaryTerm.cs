9183ec9a057a31e526fd93b202498d2b827392ca
Jason Nelson <jason@carbonmade.com>
2022-11-10 13:16:01 -0800

    Use pattern matching

--- Src/Protsyk.PMS.FullText.Core/IndexModels/DictionaryTerm.cs
+++ Src/Protsyk.PMS.FullText.Core/IndexModels/DictionaryTerm.cs
@@ -15,12 +15,12 @@ namespace Protsyk.PMS.FullText.Core
 
         public override bool Equals(object obj)
         {
-            return obj is DictionaryTerm && Equals((DictionaryTerm)obj);
+            return obj is DictionaryTerm other && Equals(other);
         }
 
         public bool Equals(DictionaryTerm other)
         {
-            return string.Equals(Key, other.Key) && Value.Equals(other.Value);
+            return string.Equals(Key, other.Key, StringComparison.Ordinal) && Value.Equals(other.Value);
         }
 
         public override int GetHashCode()
