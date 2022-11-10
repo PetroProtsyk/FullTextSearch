5f9e97fa9001d7c18003674cd99025298b9c18f8
Jason Nelson <jason@carbonmade.com>
2022-11-10 11:05:51 -0800

    Add non-allocating IPersistentStorageExtensions.AppendUtfBytes extension

---
+++ Src/Protsyk.PMS.FullText.Core/Common/Persistance/IPersistentStorageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Buffers;
+using System.Text;
+
+namespace Protsyk.PMS.FullText.Core.Common.Persistance
+{
+    internal static class IPersistentStorageExtensions
+    {
+        public static int AppendUtf8Bytes(this IPersistentStorage storage, ReadOnlySpan<char> chars)
+        {
+            var rentedBuffer = ArrayPool<byte>.Shared.Rent(Encoding.UTF8.GetMaxByteCount(chars.Length));
+
+            try
+            {
+                var byteCount = Encoding.UTF8.GetBytes(chars, rentedBuffer);
+
+                storage.WriteAll(storage.Length, rentedBuffer.AsSpan(0, byteCount));
+
+                return byteCount;
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(rentedBuffer);
+            }
+        }
+    }
+}
