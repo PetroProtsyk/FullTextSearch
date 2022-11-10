bc24eabbef43d4d577ad6db77eb046091c167cd5
Jason Nelson <jason@carbonmade.com>
2022-11-10 11:32:50 -0800

    Add non-allocating IPersistentStorageExtensions extensions to write and append Int32 and Int64 values

--- Src/Protsyk.PMS.FullText.Core/Common/Persistance/IPersistentStorageExtensions.cs
+++ Src/Protsyk.PMS.FullText.Core/Common/Persistance/IPersistentStorageExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Text;
 
 namespace Protsyk.PMS.FullText.Core.Common.Persistance
@@ -22,6 +23,38 @@ namespace Protsyk.PMS.FullText.Core.Common.Persistance
             {
                 ArrayPool<byte>.Shared.Return(rentedBuffer);
             }
+        }
+
+        public static int WriteInt32LittleEndian(this IPersistentStorage storage, long fileOffset, int value)
+        {
+            Span<byte> buffer = stackalloc byte[4];
+
+            BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+
+            storage.WriteAll(fileOffset, buffer);
+
+            return 4;
+        }
+
+        public static int WriteInt64LittleEndian(this IPersistentStorage storage, long fileOffset, long value)
+        {
+            Span<byte> buffer = stackalloc byte[8];
+
+            BinaryPrimitives.WriteInt64LittleEndian(buffer, value);
+
+            storage.WriteAll(fileOffset, buffer);
+
+            return 8;
+        }
+
+        public static int AppendInt32LittleEndian(this IPersistentStorage storage, int value)
+        {
+            return WriteInt32LittleEndian(storage, storage.Length, value);
+        }
+
+        public static int AppendInt64LittleEndian(this IPersistentStorage storage, long value)
+        {
+            return WriteInt64LittleEndian(storage, storage.Length, value);
         }
     }
 }
